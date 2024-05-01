906fb461970d266ab7f21d8899390433333ed221
Vladimir Sorokin <v.sorokin@hey.com>
2024-05-01 12:53:10 +0300

    Updated page paddings: have to looks like on print mode

--- src/app/CV/page.tsx
+++ src/app/CV/page.tsx
@@ -3,7 +3,7 @@ import { AboutMe, Contacts, Education, Experience, Languages, Skills } from "@/B
 export default function Page() {
   return (
     <main className="flex justify-center min-h-screen w-full">
-      <article className="w-[210mm] max-w-[210mm]">
+      <article className="w-[210mm] max-w-[210mm] px-[37px] print:px-0">
         <section className="flex flex-col items-center min-h-screen w-full">
           <Contacts />
           <AboutMe />
--- src/app/cover-letter/page.tsx
+++ src/app/cover-letter/page.tsx
@@ -4,7 +4,7 @@ import {  Paragraph } from "@/UI";
 export default function Page() {
   return (
     <main className="flex justify-center min-h-screen w-full">
-      <article className="w-[210mm] max-w-[210mm]">
+      <article className="w-[210mm] max-w-[210mm] px-[37px] print:px-0">
         <section className="flex flex-col items-center min-h-screen w-full">
           <Contacts variant="short" />
           {/* <CurrentDate /> */}
